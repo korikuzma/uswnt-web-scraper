74213d432ee5d3d913ea176ed90ec0e661a355c7
korikuzma <kuzma.26@osu.edu>
2020-08-12 22:32:02 -0400

    cleaned up code. updates data to mongoDB database

--- uswnt-web-scraper.py
+++ uswnt-web-scraper.py
@@ -1,65 +1,117 @@
 # Import libraries
+import sys
 import requests
 from bs4 import BeautifulSoup
+import pymongo
+from pymongo import MongoClient
 
-# Site we want to scrape
-uswnt_URL = 'https://www.ussoccer.com/players'
-players_page = requests.get(uswnt_URL)
-soup = BeautifulSoup(players_page.content, 'html.parser')
-results = soup.find(id='uswnt')
+# Initalize and declare variables
+uswnt_URL, db, collection = 'https://www.ussoccer.com/players', None, None
 
-# Get all the players div
-players = results.find_all('div', class_='PlayerThumbnail-module__playerName--2bbtZ')
+# Get the div of all players
+def get_players_div():
+    players_page = requests.get(uswnt_URL)
+    soup = BeautifulSoup(players_page.content, 'html.parser')
+    results = soup.find(id='uswnt')
 
-# Print out each player's info and stats
-for player in players:
-    player_info = (player.text).split()
-    player_num = player_info[0]
-    player_fname = (player_info[1]).replace("'", "")
-    player_lname = (player_info[2]).replace("'", "")
+    # Get all the players div
+    return results.find_all('div', class_='PlayerThumbnail-module__playerName--2bbtZ')
 
+# Get player URL
+def get_player_url(lname, fname):
     # Player URL
-    players_URL = uswnt_URL + "/" + player_lname[0].lower() + "/" + player_fname.lower() + "-" + player_lname.lower()
+    players_URL = uswnt_URL + "/" + lname[0].lower() + "/" + fname.lower() + "-" + lname.lower()
     players_page = requests.get(players_URL)
     players_info_soup = BeautifulSoup(players_page.content, 'html.parser')
-    players_info_results = players_info_soup.find(id='player-overview')
+    return players_info_soup.find(id='player-overview')
 
-    # Get player's information
-    players_info = players_info_results.find('div', class_='P1Player-module__textColumn--24s4m')
-    player_background = players_info.find_all('li')
+# Update database with data
+def update_db(position, number, fname, lname, dob, hometown, height, club, appearances, goals, assists, clean_sheets, world_cups):
+        doc = None
+        if(position != "Goalkeeper"):
+            doc = {"_id": number, "backround":{"fname": fname, "lname": lname, "position": position, "number": number, "dob": dob, "hometown": hometown, "height": height, "club": club}, "stats": {"appearances": appearances, "goals": goals, "assists": assists, "world_cups": world_cups} }
+        else:
+            doc = {"_id": number, "backround":{"fname": fname, "lname": lname, "position": position, "number": number, "dob": dob, "hometown": hometown, "height": height, "club": club}, "stats": {"appearances": appearances, "clean_sheets": clean_sheets, "world_cups": world_cups} }
 
-    # Goalkeeper's have different stats
-    is_goal_keeper = False
+        collection.update_one({"_id": number},{"$set" :doc}, upsert=True)
 
-    # Print player's full name
-    print(player_fname, player_lname)
-
-    # Print player's background information
-    for player in player_background:
-        print(player.text)
-        if(player.text == "Position Goalkeeper"):
-            is_goal_keeper = True
-
-    # Stats for each player
-    player_stats = (players_info_results.find_all('div', class_='HighlightStats-module__value--6exAY'))
-    temp = []
-    for stat in player_stats:
-        temp.append(stat.text)
-
+# Get number of world cups played by player
+def get_num_wc(players_info):
     # Number of world cups won
     num_wc = 0
     wc = players_info.find('div', class_='HighlightStats-module__starContainer--3J-I7')
     if(wc is not None):
         wc_stars = wc.findChildren("svg" , recursive=False)
         num_wc = len(wc_stars)
+    return num_wc
 
-    # Print the stats
-    print("Appearances", temp[0])
-    if(not is_goal_keeper):
-        print("Goals", temp[1])
-        print("Assists", temp[2])
-    else:
-        print("Clean Sheets", temp[1])
-    print("World Cups", num_wc)
+# Get player's info
+def get_player_info(players):
+    for player in players:
+        player_info = (player.text).split()
+        player_num = player_info[0]
+        fname = (player_info[1]).replace("'", "")
+        lname = (player_info[2]).replace("'", "")
+
+        # Player URL
+        players_info_results = get_player_url(lname, fname)
+
+        # Get player's information
+        players_info = players_info_results.find('div', class_='P1Player-module__textColumn--24s4m')
+        player_background = players_info.find_all('li')
+
+        position, number, dob, hometown, height, club = None, None, None, None, None, None
+
+        # Set player's background information
+        for info in player_background:
+            if("Position" in info):
+                position = (info.text).replace("Position ", "")
+            if("Number" in info):
+                number = (info.text).replace("Number ", "")
+            if("Date of Birth" in info):
+                dob = (info.text).replace("Date of Birth ", "")
+            if("Hometown" in info):
+                hometown = (info.text).replace("Hometown ", "")
+            if("Height" in info):
+                height = (info.text).replace("Height ", "")
+            if("Club" in info):
+                club = (info.text).replace("Club ", "")
+
+        # Stats for each player
+        player_stats = (players_info_results.find_all('div', class_='HighlightStats-module__value--6exAY'))
+        temp = []
+
+        for stat in player_stats:
+            temp.append(stat.text)
+
+        # Number of world cups won
+        num_wc = get_num_wc(players_info)
+
+        appearances, goals, assists, clean_sheets = temp[0], None, None, None
+
+        if(position != "Goalkeeper"):
+            goals, assists = temp[1], temp[2]
+        else:
+            clean_sheets = temp[1]
+        world_cups = num_wc
+
+        update_db(position, number, fname, lname, dob, hometown, height, club, appearances, goals, assists, clean_sheets, world_cups)
+
+
+if __name__ == "__main__":
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+
+    cluster = MongoClient(f"mongodb+srv://{username}:{password}@cluster0.dz1fr.mongodb.net/uswnt?ssl=true&ssl_cert_reqs=CERT_NONE&retryWrites=true&w=majority")
+
+    db = cluster["uswnt"]
+    collection = db["players"]
+
     print()
+    print("Updating database...")
 
+    players = get_players_div()
+    get_player_info(players)
+
+    print()
+    print("Finished updating database")
